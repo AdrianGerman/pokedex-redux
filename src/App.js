611e648331d70f3ca2c57973a9fdb478f6f8aca7
AdrianGerman <adrian.german1019@gmail.com>
2023-11-25 08:06:12 -0600

    immutability

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ import logo from "./statics/logo.svg";
 import "./App.css";
 
 function App() {
-  const pokemons = useSelector((state) => state.pokemons);
-  const loading = useSelector((state) => state.loading);
+  const pokemons = useSelector((state) => state.get("pokemons")).toJS();
+  const loading = useSelector((state) => state.get("loading"));
+  // const loading = false;
   const dispatch = useDispatch();
 
   useEffect(() => {
